971e9e9570bb22a6bb55fa884f61137401f9295d
Jared <91357852+jad7qt@users.noreply.github.com>
2025-09-02 23:40:43 -0400

    Add 189 rotate array

---
+++ array_string/medium/189_rotate_array/189_rotate_array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        copy = nums[:]
+        if (k == 0):
+            return
+        for i in range(len(nums)):
+            spot = (len(nums) - k + i) % len(nums)
+            nums[i] = copy[spot]
