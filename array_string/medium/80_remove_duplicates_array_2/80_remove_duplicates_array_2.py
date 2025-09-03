e8ff9c9ff0b0a5441494b83bbdda8d42cd0ca19c
jared <jad7qt@virginia.edu>
2025-09-03 19:51:35 -0400

    Change spacing on python files

--- array_string/medium/80_remove_duplicates_array_2/80_remove_duplicates_array_2.py
+++ array_string/medium/80_remove_duplicates_array_2/80_remove_duplicates_array_2.py
@@ -1,12 +1,12 @@
 class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        i = 0
-        while i < len(nums)-2:
-            # while the element 2 spaces ahead is equal, remove it
-            while i+2 < len(nums) and nums[i+2] == nums[i]:
-                nums.pop(i+2)
-            i += 1
+  def removeDuplicates(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    i = 0
+    while i < len(nums)-2:
+      # while the element 2 spaces ahead is equal, remove it
+      while i+2 < len(nums) and nums[i+2] == nums[i]:
+        nums.pop(i+2)
+      i += 1
--- two_pointer/easy/125_valid_palindrome/125_valid_palindrome.py
+++ two_pointer/easy/125_valid_palindrome/125_valid_palindrome.py
@@ -1,12 +1,12 @@
 import re
 
 class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        letter_string = re.sub(r"[^a-zA-Z0-9]", "", s).lower()
-        if letter_string == letter_string[::-1]:
-            return True
-        return False
+  def isPalindrome(self, s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    letter_string = re.sub(r"[^a-zA-Z0-9]", "", s).lower()
+    if letter_string == letter_string[::-1]:
+      return True
+    return False
