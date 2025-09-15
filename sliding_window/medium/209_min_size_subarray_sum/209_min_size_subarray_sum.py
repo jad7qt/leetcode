f1b26328763e05d1a509bdafdb4e57319ce29d7c
Jared <91357852+jad7qt@users.noreply.github.com>
2025-09-15 19:03:43 -0400

    Add problem 209 initial code

---
+++ sliding_window/medium/209_min_size_subarray_sum/209_min_size_subarray_sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+  def minSubArrayLen(self, target, nums):
+    """
+    :type target: int
+    :type nums: List[int]
+    :rtype: int
+    """
+    first = 0
+    ml = float('inf')
+    s = 0
+    for second in range(0, len(nums)):
+      s += nums[second]
+      while s >= target and first <= second:
+        # save minimum
+        ml = min(ml, second - (first-1))
+        # move first pointer up and subtract from sum
+        s -= nums[first]
+        first += 1
+    return ml if ml < float('inf') else 0
