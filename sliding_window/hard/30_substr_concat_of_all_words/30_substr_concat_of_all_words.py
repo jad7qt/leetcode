ce3e3aec564de90c7c172b6aab6623b4328bb45d
Jared <jared.dutt@daikinapplied.com>
2025-09-17 10:03:08 -0400

    Update problem 30 substring concat all words to more efficient solution

--- sliding_window/hard/30_substr_concat_of_all_words/30_substr_concat_of_all_words.py
+++ sliding_window/hard/30_substr_concat_of_all_words/30_substr_concat_of_all_words.py
@@ -1,42 +1,53 @@
 class Solution(object):
-  def findSubstring(self, s, words):
-    """
-    :type s: str
-    :type words: List[str]
-    :rtype: List[int]
-    """
-    n = len(words[0])
-    window_length = 0
-    first = 0
-    arr = []
-    d = {}
-    for word in words:
-      if word in d:
-        d[word] = d[word] + 1
-      else:
-        d[word] = 1
-      window_length += len(word)
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        wordLen = len(words[0])
+        numWords = len(words)
+        totalLen = wordLen * numWords
+        arr = []
+        d = {}
 
-    # Determine if a string is concatenation of all words
-    def isConcat(substr):
-      d_temp = d.copy()
-      first_substr = 0
-      section = substr[first_substr:first_substr+n]
-      while section in d_temp and d_temp[section] > 0:
-        d_temp[section] = d_temp[section] - 1
-        if first_substr+n == window_length:
-          return True
-        first_substr += n
-        section = substr[first_substr:first_substr+n]
-      return False
-    
-    # Iterate over all possible substrings
-    for last in range(window_length-1, len(s)):
-      # Check if substring is concatenation of all words
-      if isConcat(s[first:last+1]):
-        arr.append(first)
+        # Create frequency map
+        for word in words:
+            if word in d:
+                d[word] = d[word] + 1
+            else:
+                d[word] = 1
+        
+        for initialStart in range(wordLen):
+            d_temp = d.copy()
+            # check each word
+            first, second = initialStart, initialStart + wordLen
+            substrFirst = initialStart
 
-      # Increment first position
-      first += 1
-  
-    return arr
+            for second in range(initialStart + wordLen, len(s)+1, wordLen):
+                currWord = s[substrFirst:second]
+                if currWord in d_temp:
+                    if d_temp[currWord] > 0:
+                        # word in list and frequency not violated, continue after decrementing frequency
+                        d_temp[currWord] = d_temp[currWord] - 1
+
+                        # if current length is equal to total length, add index to arr
+                        if second - first == totalLen:
+                            # remove word from beginning and add to arr
+                            arr.append(first)
+                            removedWord = s[first:first+wordLen]
+                            d_temp[removedWord] = d_temp[removedWord] + 1
+                            first += wordLen
+                    else:
+                        # word violated frequency, remove from left until no longer violated
+                        while d_temp[currWord] < 1:
+                            removedWord = s[first:first+wordLen]
+                            d_temp[removedWord] = d_temp[removedWord] + 1
+                            first += wordLen
+                        d_temp[removedWord] = d_temp[removedWord] - 1
+                else:
+                    # word not in list, reset d_temp and move to next word
+                    first = second
+                    d_temp = d.copy()
+                substrFirst += wordLen
+        return arr
